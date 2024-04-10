e6654273fcdc47b78df7ccd1537f044140180269
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-10 23:27:39 +0000

    add swagger on user routes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -7,10 +7,59 @@ const upload = multer();
 const router = express.Router();
 
 /**
- * 
+ * @swagger
+ * /signup:
+ *  post:
+ *    summary: Inscription
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *    responses:
+ *      200:
+ *        description: Inscription reussie
+ *      400:
+ *        description: Erreur
+ *      500:
+ *        description: Erreur server
  */
 router.post("/signup", upload.any(), signup);
 
+/**
+ * @swagger
+ * /login:
+ *  post:
+ *    summary: Connexion
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              password:
+ *                type: string
+ *    responses:
+ *      200:
+ *        description: Connexion reussie
+ *      400:
+ *        description: Erreur
+ *      500:
+ *        description: Erreur server
+ */
 router.post("/login", upload.any(), login);
 
 export default router;
